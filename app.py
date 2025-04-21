8df24d06c2cd88e9270218055a693e8d81044301
Jatin Wig <wigjatin2@gmail.com>
2025-04-21 22:36:49 +0530

    more changes

--- app.py
+++ app.py
@@ -4,27 +4,12 @@ import nltk
 import re
 import string
 import os
-import time
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-nltk_data_path = os.path.join(os.path.expanduser("~"), "nltk_data")
-os.makedirs(nltk_data_path, exist_ok=True)
-nltk.data.path.append(nltk_data_path)
-
-def download_nltk_resource(resource):
-    try:
-        nltk.data.find(resource)
-    except LookupError:
-        try:
-            nltk.download(resource.split('/')[-1], download_dir=nltk_data_path)
-            nltk.data.path.append(nltk_data_path)
-        except Exception as e:
-            st.error(f"Failed to download NLTK resource {resource}: {str(e)}")
-            st.stop()
-
-for resource in ['tokenizers/punkt', 'corpora/stopwords']:
-    download_nltk_resource(resource)
+nltk.download('punkt', quiet=True)
+nltk.download('stopwords', quiet=True)
+nltk.download('punkt_tab', quiet=True)
 
 try:
     model = joblib.load('model.pkl')
